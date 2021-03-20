5ca507ee25e10d6b6118e51aca9a5c140c032bac
dneb <ya.dneb@yandex.ru>
2021-03-21 01:30:22 +0500

    test filds

--- resources/js/components/content/Auth.vue
+++ resources/js/components/content/Auth.vue
@@ -18,8 +18,8 @@ export default {
     },
     data() {
         return {
-            login: '',
-            password: '',
+            login: 'login',
+            password: 'password',
         }
     },
     computed: {
