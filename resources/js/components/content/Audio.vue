3527b7284e69bf2cdd287a7d507547346b57793b
dnebik <ya.dneb@yandex.ru>
2021-03-20 13:15:50 +0300

    underline

---
+++ resources/js/components/content/Audio.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Audio</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Audio"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/content/Monitoring.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Monitoring</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Monitoring"
+}
+</script>
+
+<style scoped>
+
+</style>
