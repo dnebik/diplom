fc32e0e5633051471fc17262cda8fc2e03f859e3
dnebik <ya.dneb@yandex.ru>
2021-03-20 14:00:13 +0300

    mobile nav

--- resources/js/components/content/helpers/Navigation.vue
+++ resources/js/components/content/helpers/Navigation.vue
@@ -1,11 +1,20 @@
 <template>
     <div class="nav">
 
-        <div v-if="currentRoute === 'docs'">
-
+        <div class="body" v-if="currentRoute === '/documents'">
+            <router-link :to="{name: 'docs'}">
+                <div class="name">Мои документы</div>
+            </router-link>
+            <router-link :to="{name: 'requests'}">
+                <div class="name">Запросы</div>
+            </router-link>
+            <router-link :to="{name: 'history'}">
+                <div class="name">История просмотров</div>
+            </router-link>
+            <router-link :to="{name: 'search'}">
+                <div class="name">Поиск документов</div>
+            </router-link>
         </div>
-
-
 
     </div>
 </template>
@@ -15,7 +24,7 @@ export default {
     name: "Navigation",
     computed: {
         currentRoute() {
-            return this.$route.matched[0].name;
+            return this.$route.matched[0].path;
         }
     }
 }
@@ -23,4 +32,29 @@ export default {
 
 <style scoped lang="sass">
 
+.nav
+    @media (max-width: 414px)
+        .body
+            background-color: #017174
+            padding-top: 6px
+            padding-bottom: 6px
+
+        a
+            font-size: 22px
+            width: 100%
+            height: 45px
+            justify-content: right
+            align-items: center
+
+            .name
+                margin-right: 51px
+
+            &.router-link-active
+                background-color: #028F91
+                color: #ffffff
+            &:not(.router-link-active)
+                color: #48ACAD
+
+    a
+        display: flex
 </style>
