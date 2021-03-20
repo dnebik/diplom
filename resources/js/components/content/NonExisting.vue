4750358a045e7b357fa0be28ab7d42b4f50709fa
dnebik <ya.dneb@yandex.ru>
2021-03-20 13:05:44 +0300

    404

--- resources/js/components/content/NonExisting.vue
+++ resources/js/components/content/NonExisting.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>
-
+    <div class="body">
+        <img :src="$root.$data.path + '/images/ghost.svg'" alt="404 ghost">
+        <div class="text">
+            <h1>404</h1>
+            <p>Куда мы попали?</p>
+            <router-link to="documents">Вернуться на главную</router-link>
+        </div>
     </div>
 </template>
 
@@ -10,6 +15,29 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="sass">
+    .body
+        display: flex
+        flex-wrap: wrap
+        margin: 120px auto 80px
+        width: max-content
+        color: #A3D5D6
 
+        img
+            width: 100px
+            margin-right: 30px
+            margin-top: 20px
+
+        .text
+            width: max-content
+            h1
+                font-size: 102px
+                margin: 0
+            p
+                font-size: 20px
+                font-weight: bold
+                margin: -20px 0 5px
+
+            a
+                text-decoration: underline
 </style>
