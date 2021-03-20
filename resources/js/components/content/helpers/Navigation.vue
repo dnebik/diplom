e732d0f33366eb512452a9db1171d2b79149fb5f
dneb <ya.dneb@yandex.ru>
2021-03-20 23:34:46 +0500

    navigate

--- resources/js/components/content/helpers/Navigation.vue
+++ resources/js/components/content/helpers/Navigation.vue
@@ -1,11 +1,20 @@
 <template>
     <div class="nav">
 
-        <div v-if="currentRoute === 'docs'">
-
+        <div class="body" v-if="route === '/documents'">
+            <router-link :to="{name: 'docs'}">
+                <div class="name">Мои документы</div>
+            </router-link>
+            <router-link :to="{name: 'requests'}">
+                <div class="name">Запросы</div>
+            </router-link>
+            <router-link :to="{name: 'history'}">
+                <div class="name">История просмотров</div>
+            </router-link>
+            <router-link :to="{name: 'search'}">
+                <div class="name">Поиск документов</div>
+            </router-link>
         </div>
-
-
 
     </div>
 </template>
@@ -13,9 +22,25 @@
 <script>
 export default {
     name: "Navigation",
-    computed: {
-        currentRoute() {
-            return this.$route.matched[0].name;
+    data() {
+        return {
+            route: ''
+        }
+    },
+    mounted() {
+        this.update();
+    },
+    watch: {
+        $route: {
+            handler(to, from) {
+                this.update();
+            }
+        },
+    },
+    methods: {
+        update() {
+            this.route = this.$route.matched[0].path;
+            this.$emit('updated');
         }
     }
 }
@@ -23,4 +48,29 @@ export default {
 
 <style scoped lang="sass">
 
+.nav
+    @media (max-width: 414px)
+        .body
+            background-color: #017174
+            padding-top: 6px
+            padding-bottom: 6px
+
+        a
+            font-size: 22px
+            width: 100%
+            height: 45px
+            justify-content: right
+            align-items: center
+
+            .name
+                margin-right: 51px
+
+            &.router-link-active
+                background-color: #028F91
+                color: #ffffff
+            &:not(.router-link-active)
+                color: #48ACAD
+
+    a
+        display: flex
 </style>
