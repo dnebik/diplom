35faef55f740bc10fe9ab13be732bbba8787f442
dnebik <ya.dneb@yandex.ru>
2021-03-20 10:56:04 +0300

    styles

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -13,27 +13,22 @@ export default {
 }
 </script>
 
-<style scoped>
-    .line {
-        position: relative;
-        background-color: #028F91;
-    }
-    @media (min-width: 1024px) {
-        .line {
-            border-radius: 36px;
-            margin: 24px 41px 56px 64px;
-        }
-    }
-    @media (min-width: 414px) {
-        .line {
-            height: 72px;
-            margin: 24px 0 56px 0;
-        }
-    }
-    .logo {
-        width: 125px;
-        position: absolute;
-        left: 0;
+<style scoped lang="sass">
+    .line
+        position: relative
+        background-color: #028F91
+        @media (min-width: 1024px)
+            border-radius: 36px
+            margin: 24px 41px 56px 64px !important
+        @media (min-width: 414px)
+            height: 72px
+        @media (max-width: 414px)
+            height: 67px
+        @media (min-width: 414px) and (max-width: 1024px)
+            margin: 24px 0 56px 0
 
-    }
+        .logo
+            width: 125px
+            position: absolute
+            left: 0
 </style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Diplom</title>
+
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
 
