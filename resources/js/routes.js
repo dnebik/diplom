52dff70a8f4718b8f5e2f0fe3ade58740fa70b3d
dnebik <ya.dneb@yandex.ru>
2021-03-22 11:53:43 +0300

    fix documents root path

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -19,9 +19,9 @@ const routes = [
     {
         path: '/documents',
         component: Documents,
-        redirect: '/my_docs',
         meta: {title: 'Документы', guest: false},
         children: [
+            { path: '', redirect: {name: 'docs'} },
             { path: 'my_docs', name: 'docs', meta: {title: 'Мои Документы'}, component: MyDocuments },
             { path: 'requests', name: 'requests', meta: {title: 'Запросы'},component: Requests },
             { path: 'history', name: 'history', meta: {title: 'История'},component: History },
