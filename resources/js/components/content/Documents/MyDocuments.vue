705a8bc1153cb2bda6d2719872e95bd56a155d37
dnebik <ya.dneb@yandex.ru>
2021-04-22 09:13:44 +0300

    fixes

--- resources/js/components/content/Documents/MyDocuments.vue
+++ resources/js/components/content/Documents/MyDocuments.vue
@@ -1,15 +1,40 @@
 <template>
-    <div>
-        <h1>MyDocs</h1>
+    <div class="my-documents-container">
+<!--        <DatePicker></DatePicker>-->
+        <ListItem id="IAV14AMF2" date="21.04.2012" fio="Небренчин Д.С." comment="Коммандировка" status="-"/>
+        <ListItem id="C6MSHXWDQ"  date="01.03.2012" fio="Погонялов В.А." comment="" status="Отказано"/>
+        <ListItem id="INO1LIQQY"  date="15.11.2021" fio="Васичкин Д.С." comment="Отпуск" status="Согласован"/>
+        <ListItem id="IAV14AMF2" date="21.04.2012" fio="Небренчин Д.С." comment="Коммандировка" status="-"/>
+        <ListItem id="C6MSHXWDQ"  date="01.03.2012" fio="Погонялов В.А." comment="" status="Отказано"/>
+        <ListItem id="INO1LIQQY"  date="15.11.2021" fio="Васичкин Д.С." comment="Отпуск" status="Согласован"/>
+        <ListItem id="IAV14AMF2" date="21.04.2012" fio="Небренчин Д.С." comment="Коммандировка" status="-"/>
+        <ListItem id="C6MSHXWDQ"  date="01.03.2012" fio="Погонялов В.А." comment="" status="Отказано"/>
+        <ListItem id="INO1LIQQY"  date="15.11.2021" fio="Васичкин Д.С." comment="Отпуск" status="Согласован"/>
+        <ListItem id="IAV14AMF2" date="21.04.2012" fio="Небренчин Д.С." comment="Коммандировка" status="-"/>
+        <ListItem id="C6MSHXWDQ"  date="01.03.2012" fio="Погонялов В.А." comment="" status="Отказано"/>
+        <ListItem id="INO1LIQQY"  date="15.11.2021" fio="Васичкин Д.С." comment="Отпуск" status="Согласован"/>
+        <ListItem id="IAV14AMF2" date="21.04.2012" fio="Небренчин Д.С." comment="Коммандировка" status="-"/>
+        <ListItem id="C6MSHXWDQ"  date="01.03.2012" fio="Погонялов В.А." comment="" status="Отказано"/>
+        <ListItem id="INO1LIQQY"  date="15.11.2021" fio="Васичкин Д.С." comment="Отпуск" status="Согласован"/>
+        <ListItem id="IAV14AMF2" date="21.04.2012" fio="Небренчин Д.С." comment="Коммандировка" status="-"/>
+        <ListItem id="C6MSHXWDQ"  date="01.03.2012" fio="Погонялов В.А." comment="" status="Отказано"/>
+        <ListItem id="INO1LIQQY"  date="15.11.2021" fio="Васичкин Д.С." comment="Отпуск" status="Согласован"/>
     </div>
 </template>
 
 <script>
+import ListItem from "../UI/ListItem";
+// import DatePicker from "vue-datetime";
 export default {
-    name: "MyDocuments"
+    name: "MyDocuments",
+    components: {
+        ListItem,
+        // DatePicker
+    }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="sass">
+    .my-documents-container
+        flex: auto
 </style>
