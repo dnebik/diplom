a64a712655790413c214d019a02808617d90f7da
dnebik <ya.dneb@yandex.ru>
2021-04-28 09:18:34 +0300

    sheet code made YEEEY

--- resources/js/components/content/UI/DocumentFilter.vue
+++ resources/js/components/content/UI/DocumentFilter.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="form">
-        <div v-if="['history', 'search', 'docs'].includes(routeName)" style="margin-top: 50px">
+    <div class="form" ref="form">
+        <div v-if="['history', 'search', 'docs'].includes(routeName)" style="margin-top: 50px;">
             <h2>Фильтр:</h2>
             <form @submit.prevent="Submit" autocomplete="off">
-                <InputBox v-model="filter.like" label="Найти"/>
-                <DateRange v-model="filter.range"/>
+                <InputBox style="width: inherit; box-sizing: border-box" v-model="filter.like" label="Найти" ref="i"/>
+                <DateRange v-model="filter.range" ref="z"/>
                 <button class="btn submit" type="submit">Подтвердить</button>
             </form>
         </div>
@@ -14,14 +14,14 @@
                 <InputBox v-model="filter.like" label="Найти"/>
                 <label class="select-label">
                     <span>Статус: </span>
-                    <select v-model="status">
+                    <select v-model="filter.status">
                         <option value="0">Все</option>
                         <option value="1">Новый</option>
                         <option value="3">Решен</option>
                         <option value="2">Просмотрен</option>
                     </select>
                 </label>
-                <DateRange v-model="filter.range"/>
+                <DateRange style="width: 100%; box-sizing: content-box" v-model="filter.range"/>
                 <button class="btn submit" type="submit">Подтвердить</button>
             </form>
         </div>
@@ -40,10 +40,11 @@ export default {
     data() {
         return {
             filter: {
-                like: null,
+                like: '',
                 range: null,
                 status: 0,
-            }
+            },
+            size: 0,
         }
     },
     props: {
@@ -52,14 +53,41 @@ export default {
             required: true,
         }
     },
+    watch: {
+        '$refs': {
+            handler: 'refsChange',
+            deep: true,
+            immediate: true,
+        }
+    },
+    mounted() {
+        // setInterval(() => {console.log(this.$refs)}, 1000);
+    },
     methods: {
         Submit() {
             this.$emit('submit', this.filter);
+        },
+        refsChange() {
+            setTimeout(() => {
+                if (this.$refs.z) this.size = this.$refs.z.$el.clientWidth;
+                this.$refs.form.style.width = this.size + 'px';
+            },1)
         }
     }
 }
 </script>
 
 <style scoped lang="sass">
-
+    .form
+        box-sizing: border-box
+        display: block !important
+        & > div
+            display: flex
+            flex-direction: column
+            justify-content: center
+            & > form
+                display: flex
+                flex-direction: column
+                justify-content: center
+                align-items: end
 </style>
