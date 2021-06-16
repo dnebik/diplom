e71ad73b3a6afea7ae7dede6b4768b04c11c1689
dnebik <ya.dneb@yandex.ru>
2021-06-16 11:58:34 +0300

    + animation

--- resources/js/components/content/UI/DroppingList.vue
+++ resources/js/components/content/UI/DroppingList.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="droping-list">
+    <div class="droping-list" ref="body">
         <div class="top-panel" @click="open">{{label}}</div>
-        <div class="list collapsed" style="max-height: 0" ref="list">
+        <div :class="['list', 'collapsed']" style="max-height: 0; overflow-y: hidden" ref="list">
             <slot></slot>
         </div>
         <Modal :view-modal="open_modal" @close="open_modal = false">
@@ -35,8 +35,17 @@ export default {
                 let list = this.$refs.list;
                 if (list.classList.contains('collapsed')) {
                     list.style.maxHeight = (list.scrollHeight > 365 ? 365 : list.scrollHeight) + 'px';
+                    list.style.padding = '10px 8px 3px 8px';
+                    this.$refs.body.style.boxShadow = '0px 0px 5px rgba(72, 172, 173, 0.47)';
+                    setTimeout(() => {
+                        list.style.overflowY = 'auto'
+                    }, 200)
                 } else {
+                    list.style.overflowY = 'hidden'
                     list.style.maxHeight = 0;
+                    setTimeout(() => {
+                        list.style.padding = '0';
+                    }, 155)
                 }
                 list.classList.toggle('collapsed');
             }
@@ -47,7 +56,9 @@ export default {
 
 <style scoped lang="sass">
     .droping-list
+        width: 100%
         margin-bottom: 10px
+        transition: 0.2s
         .top-panel
             -webkit-touch-callout: none
             -webkit-user-select: none
@@ -64,8 +75,8 @@ export default {
             font-size: 16px
             cursor: pointer
         .list
-            transition: 0.2s
+            transition: max-height 0.2s
             overflow-x: hidden
-            overflow-y: auto
+
 
 </style>
