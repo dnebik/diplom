0d3e3fc96aa5cd574569d1e79d2ff3dbdbca5b63
dnebik <ya.dneb@yandex.ru>
2021-06-16 12:06:21 +0300

    ~ view item

---
+++ resources/js/components/content/UI/ViewDocumentItem.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="view-document-item">
+        <span class="fio">{{name}}</span>
+        <div class="datetime">
+            <span class="date">{{date}}</span>
+            <span class="time">{{time}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ViewDocumentItem",
+    props: {
+        name: String,
+        date: String,
+        time: String,
+    }
+}
+</script>
+
+<style scoped lang="sass">
+    .view-document-item
+        margin-bottom: 18px
+        border-radius: 3px
+        background-color: #028F91
+        color: #ffffff
+        display: flex
+        padding: 5px 6px 2px 6px
+        z-index: 5
+        box-shadow: 0 1px 1px rgba(0, 0, 0, 0.25)
+        .fio
+            font-size: 16px
+        .datetime
+            margin-left: auto
+            display: flex
+            flex-direction: column
+            align-items: end
+            .date
+                font-size: 10px
+            .time
+                font-size: 8px
+                opacity: 0.8
+</style>
